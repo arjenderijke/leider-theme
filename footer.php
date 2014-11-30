b00d675b993da7e2e27300b5852a8ae4c305e569
Arjen de Rijke <arjenderijke@gmail.com>
2014-11-30 10:21:55 +0100

    add empty footer file

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+ * Theme Name	: leider
+ * @file          footer.php
+ * @package       leider
+ * @author        Arjen de Rijke
+ * @copyright     2014 Hammock
+ * @license       license.txt
+ * @filesource    wp-content/themes/leider/footer.php
+ */
+?>
+
+<!-- closing of the footer -->
+<?php wp_footer(); ?> 
+</body>
+</html>
