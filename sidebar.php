76e50bcf4cb2cb978640525cdb40c9aa374f03c0
Arjen de Rijke <arjenderijke@gmail.com>
2014-12-06 16:02:13 +0100

    put the line on a different level and edit the sidebar

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+ * Theme Name	: leider
+ * @file          footer.php
+ * @package       leider
+ * @author        Arjen de Rijke
+ * @copyright     2014 Hammock
+ * @license       license.txt
+ * @filesource    wp-content/themes/leider/sidebar.php
+ */
+?>
+
+<div class="span4 sidebar">
+  <?php if ( !dynamic_sidebar('sidebar-primary') ) : ?>		
+  <?php endif;?>
+</div>
