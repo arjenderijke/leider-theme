131453da8149c128abe572a239a6f4198902d6b8
Arjen de Rijke <arjenderijke@gmail.com>
2015-01-04 13:45:03 +0100

    try again with different column

--- index-testimonials.php
+++ index-testimonials.php
@@ -24,35 +24,15 @@ $template_uri=get_template_directory_uri(). '/images/default' ;
 					</div>
 				</div>
 		</div>
-		<div class="span6 recent_blog">			
-			<h2><?php if($current_options['recent_blog_title']!='') { echo $current_options['recent_blog_title'];} ?><br><span><?php if($current_options['recent_blog_description']!='') { echo $current_options['recent_blog_description'];} ?></span></h2>
+		<div class="span6 recent_blog">
+		     <h2>Facebook</h2>
 			<div class="row">
-				<?php	$args = array( 'numberposts' => '4' );
-						$recent_posts = wp_get_recent_posts( $args );
-						if($recent_posts)
-						{ foreach( $recent_posts as $recent )
-							{ ?><div class="span3">
-										<div id="recent_blog_cols" class="media">
-										<?php if(get_post_thumbnail_id($recent["ID"])!='') {?>
-										<?php 	$defalt_arg=array('class'=>'media-object img-circle pull-left recent_blog_thumb');
-												echo get_the_post_thumbnail($recent["ID"],'recent-blog-thumb', $defalt_arg); ?>
-											<?php } else { ?>
-											<img alt="webriti" src="<?php echo $template_uri .'/testimonial.jpg'; ?>"  class="media-object img-circle pull-left recent_blog_thumb">
-											<?php } ?>
-											<div class="media-body">
-											<p><?php echo '<a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a>'; ?></p>
-											<span><?php echo date("M d. Y", strtotime($recent['post_modified'])); ?></span>
-											</div>
-										</div>
-								</div>		
-								<?php } } else { ?>
-								<div class="span3">
-									<div class="media-body">
-									<p><?php _e("NO POST TO DISPLAY...",'busi_prof');?></p>
-									</div>
-								</div>
-								<?php } ?>	
-			</div>	
-		</div>
+				     <div class="span3">
+				     <div class="media-body">
+				     <p><?php _e("NO POST TO DISPLAY...",'busi_prof');?></p>
+				     </div>
+				     </div>
+                       </div>
+                </div>
 	</div>			
 </div>
